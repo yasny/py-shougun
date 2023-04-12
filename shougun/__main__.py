bf64b4cfef15ff79ba19a573cf1ef74ad0697823
Ian Walker <iwalker@redhat.com>
2023-04-12 10:44:32 +0900

    update logging initialization

--- shougun/__main__.py
+++ shougun/__main__.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import sys
 from dateutil.parser import parse as dateutil_parse  # type: ignore
 from .parser import thread_dumps
 from .output.html import HTMLStaticSite
@@ -11,6 +10,15 @@ class DateTimeParser(argparse.Action):
         setattr(namespace, self.dest, dateutil_parse(values))
 
 
+def initialize_logging(log_level, name, log_format=None, date_format=None):
+    if log_format is None:
+        log_format = "%(levelname)-8s %(message)s"
+    logging.basicConfig(level=log_level, format=log_format, datefmt=date_format)
+    logger = logging.getLogger(None)
+    logger.name = name
+    return logger
+
+
 def main():
     parser = argparse.ArgumentParser(description="Generate pretty output from jstack thread dumps")
     parser.add_argument("--exclude", help="Thread names to exclude (regex)")
@@ -19,22 +27,27 @@ def main():
     parser.add_argument("-te", "--end-time", action=DateTimeParser, help="End date time")
     parser.add_argument("--stack-trace", action=argparse.BooleanOptionalAction, default=True)
     parser.add_argument("-c", "--max-count", type=int, help="max count", default=-1)
+    parser.add_argument("-v", "--verbose", action="count", help="verbosity level")
     parser.add_argument("filename", help="Thread dump file")
 
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.INFO, format="%(levelname)-8s %(message)s", stream=sys.stderr)
-    logging.getLogger(None).name = "shougun"
+    if args.verbose == 1:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
 
-    output = HTMLStaticSite(timestamp_fmt="%H:%M:%S", output_dir="output")
+    initialize_logging(log_level, "shougun")
 
-    for current_thread_dump in thread_dumps(args.filename,
-                                            start_datetime=args.start_time,
-                                            end_datetime=args.end_time,
-                                            thread_name_filter_exclude=args.exclude,
-                                            thread_name_filter_include=args.include,
-                                            max_count=args.max_count):
-        output.add(current_thread_dump)
+    site = HTMLStaticSite(timestamp_fmt="%H:%M:%S", output_dir="output", include_stacktrace=args.stack_trace)
+
+    for thread_dump in thread_dumps(args.filename,
+                                    start_datetime=args.start_time,
+                                    end_datetime=args.end_time,
+                                    thread_name_filter_exclude=args.exclude,
+                                    thread_name_filter_include=args.include,
+                                    max_count=args.max_count):
+        site.add(thread_dump)
 
     logging.info("Building site to output directory.")
-    output.build()
+    site.build()
